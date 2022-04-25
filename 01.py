30d728a20380845578c535f4952dcde593f606f6
MaybeRick <103020575+MaybeRick@users.noreply.github.com>
2022-04-25 16:36:23 +0800

    01.py
    read txt file and Sort

---
+++ 01.py
@@ -0,0 +1,34 @@
+from genericpath import exists
+
+# Read the file, put each line into an array
+class readtxt:
+	def readFileToArray(filename):
+		# first version
+		lines = []
+		for line in open(filename):
+			lines.append(line)
+		return lines
+	
+	#main
+print('What .txt file would you like to sort?')
+fileToSort = input('File Name: ')
+	
+	# check if properly formatted and the file exists
+while not fileToSort.lower().endswith('.txt') or not exists(fileToSort):
+	fileToSort = input('File Name: ')
+	
+	# sort the file, line based
+elements = readtxt(fileToSort)
+elements.sort()
+	
+# creates output file in the format 'fileName + _Sorted.txt'
+outputName = fileToSort.split('.', -1)[0] + ('_Sorted.txt')
+print(outputName)
+	
+	# writes the sorted elements to output file (line based)
+with open(outputName, 'w') as out:
+	for element in elements:
+		if element.endswith('\n'):
+			out.write(element)
+		else:
+				out.write(element + '\n')
